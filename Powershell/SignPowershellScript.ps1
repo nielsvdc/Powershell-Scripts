a7aad7ace9edcbb7a2906913b880ab607bc5c14b
Niels van de Coevering | Kresco IT <nvandecoevering@kresco.nl>
2020-10-23 12:25:32 +0200

    Added comments

--- Powershell/SignPowershellScript.ps1
+++ Powershell/SignPowershellScript.ps1
@@ -14,13 +14,21 @@ $scriptName = "MyScript.ps1"
 # Leave this as-is, if you don't know what you are doing
 $certPath = "Cert:\LocalMachine\Root"
 
+# Create variable for full path to Powershell script
 $filePath = Join-Path -Path $scriptPath -ChildPath $scriptName
+# If exist, get a code signing certificate
 $cert = Get-ChildItem -Path $certPath -CodeSigningCert
+
+# Check of a code signing certificate exists
 if (-not($cert)) {
+    # Show error message when code signing certificate does not exist
     Write-Host "Self-signed code signing certificate not found in: $certPath.`nLocation should be 'Cert:\LocalMachine\Root'. Please create a self-signed certificate first." -BackgroundColor Red
 }
 else {
+    # Sign the Powershell script using the code signing certificate
     $result = Set-AuthenticodeSignature -FilePath $filePath -Certificate $cert
+
+    # Show result
     $result
     if ($result.Status -ne 'Valid') {
         Write-Host "Failed signing Powershell script: $filePath" -BackgroundColor Red
